7521c3e9daa64524cfb4d36b125ea1fb500e24d9
Nick Janssen <info@nickjanssen.com>
2015-03-26 17:08:12 +0900

    more wip

--- client/game/ui/main-menu/register/register.js
+++ client/game/ui/main-menu/register/register.js
@@ -1,5 +1,6 @@
 angular
 	.module('game.ui.main-menu.register', [
+		'engine.util',
 		'ui.router'
 	])
 	.config(['$stateProvider', function ($stateProvider) {
@@ -7,15 +8,29 @@ angular
 
 		$stateProvider.state('main-menu.register', {
 			templateUrl: 'client/game/ui/main-menu/register/register.ng.html',
-			controller: ['$scope', '$state', function ($scope, $state) {
+			controller: ['$scope', '$state', 'Util', function ($scope, $state, Util) {
 
 				$scope.register = function() {
+					Accounts.createUser({
+						username: $scope.username,
+						password: $scope.password,
+						email: $scope.email
+					}, function (err) {
+						if (err) {
+							throw err;
+						}
+						else {
+							$state.go('main-menu.play-mode');
+						}
+					});
 					// GameService.enterGame($scope.nickname);
 				};
 
 				$scope.cancel = function () {
 					$state.go('main-menu.play-mode');
-				}
+				};
+
+				$scope.showPassword = Util.isMobile();
 			}]
 		});
 	}]);
