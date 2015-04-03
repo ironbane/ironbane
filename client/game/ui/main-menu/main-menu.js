50269bd4aafc27a0981cd9830d1177ec01057177
Ben Sparks <bsparks@unicon.net>
2015-04-02 21:34:58 -0700

    file cleanups

--- client/game/ui/main-menu/main-menu.js
+++ client/game/ui/main-menu/main-menu.js
@@ -6,16 +6,16 @@ angular
         'game.ui.main-menu.login',
         'game.ui.main-menu.register',
     ])
-    .config(['$stateProvider', function ($stateProvider) {
+    .config(['$stateProvider', function($stateProvider) {
         'use strict';
 
         $stateProvider.state('main-menu', {
             templateUrl: 'client/game/ui/main-menu/main-menu.ng.html',
             abstract: true,
-            controller: ['$scope', function ($scope) {
-            	$scope.logout = function ()  {
-            		Meteor.logout();
-            	};
+            controller: ['$scope', function($scope) {
+                $scope.logout = function() {
+                    Meteor.logout();
+                };
             }]
         });
     }]);
