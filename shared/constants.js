49f7ae847b0119f728ed7151318a3627847f4da9
Ben Sparks <bsparks@unicon.net>
2015-04-02 21:29:22 -0700

    move version string to a constant

--- shared/constants.js
+++ shared/constants.js
@@ -1,26 +1,26 @@
 ironbaneConstants = {
-	world: {
-		mainMenuLevel: 'tower-of-doom',
-		startLevel: 'tower-of-doom',
-		startPosition: [-15, 2, 12],
-		startRotation: [0, Math.PI - 0.3, 0]
-	},
-	characterParts: {
-		male: {
-			skin: [1000, 1001, 1002, 1003, 1004],
-			hair: [1000, 1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009],
-			eyes: [1000, 1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009]
-		},
-		female: {
-			skin: [1010, 1011, 1012, 1013, 1014],
-			hair: [1010, 1011, 1012, 1013, 1014, 1015, 1016, 1017, 1018, 1019],
-			eyes: [1010, 1011, 1012, 1013, 1014, 1015, 1016, 1017, 1018, 1019]
-		}
-	},
-	rules: {
-		maxCharactersAllowed: 3,
-		minCharNameLength: 2,
-		maxCharNameLength: 15
-	}
+    GAME_VERSION: '0.4.0 pre-alpha',
+    world: {
+        mainMenuLevel: 'tower-of-doom',
+        startLevel: 'tower-of-doom',
+        startPosition: [-15, 2, 12],
+        startRotation: [0, Math.PI - 0.3, 0]
+    },
+    characterParts: {
+        male: {
+            skin: [1000, 1001, 1002, 1003, 1004],
+            hair: [1000, 1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009],
+            eyes: [1000, 1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009]
+        },
+        female: {
+            skin: [1010, 1011, 1012, 1013, 1014],
+            hair: [1010, 1011, 1012, 1013, 1014, 1015, 1016, 1017, 1018, 1019],
+            eyes: [1010, 1011, 1012, 1013, 1014, 1015, 1016, 1017, 1018, 1019]
+        }
+    },
+    rules: {
+        maxCharactersAllowed: 3,
+        minCharNameLength: 2,
+        maxCharNameLength: 15
+    }
 };
-
