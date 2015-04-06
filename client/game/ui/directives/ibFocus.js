fad6e03fe23b074227b85f62505f6a0c56d440bc
Ben Sparks <bsparks@unicon.net>
2015-04-06 09:09:47 -0700

    ng-annotate apparently isnt working; fix for chat focus

--- client/game/ui/directives/ibFocus.js
+++ client/game/ui/directives/ibFocus.js
@@ -1,22 +1,25 @@
 angular.module('game.ui.directives')
-    .directive('ibFocus', function($timeout) {
-        'use strict';
+    .directive('ibFocus', [
+        '$timeout',
+        function($timeout) {
+            'use strict';
 
-        return {
-            link: function(scope, element, attrs) {
-                scope.$watch(attrs.ibFocus, function(val) {
-                    if (angular.isDefined(val) && val) {
-                        $timeout(function() {
-                            element[0].focus();
-                        });
-                    }
-                }, true);
+            return {
+                link: function(scope, element, attrs) {
+                    scope.$watch(attrs.ibFocus, function(val) {
+                        if (angular.isDefined(val) && val) {
+                            $timeout(function() {
+                                element[0].focus();
+                            });
+                        }
+                    }, true);
 
-                element.bind('blur', function() {
-                    if (angular.isDefined(attrs.ibFocusLost)) {
-                        scope.$apply(attrs.ibFocusLost);
-                    }
-                });
-            }
-        };
-    });
+                    element.bind('blur', function() {
+                        if (angular.isDefined(attrs.ibFocusLost)) {
+                            scope.$apply(attrs.ibFocusLost);
+                        }
+                    });
+                }
+            };
+        }
+    ]);
