dc648d0b617c98416878e46b125d855a80583f97
Nick Janssen <info@nickjanssen.com>
2015-04-06 10:23:44 +0900

    remove unused spawner component data for now

--- client/game/prefabs/spawnpoint.js
+++ client/game/prefabs/spawnpoint.js
@@ -1,11 +1,5 @@
 angular.module('game.prefabs.spawnpoint', [])
     .constant('SpawnPointPrefab', {
         components: {
-            'spawner': {
-                type: 'NPC',
-                spawns: ['Bunny'],
-                spawnRate: 100,
-                spawnCount: 10
-            }
         }
     });
