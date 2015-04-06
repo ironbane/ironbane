d254d6d28584e5846d43f5266f526c1137a461d7
Ben Sparks <bsparks@unicon.net>
2015-04-06 00:33:17 -0700

    track level and date for achievements

--- client/components/gameplay/achievement.js
+++ client/components/gameplay/achievement.js
@@ -1,65 +1,65 @@
 angular.module('components.gameplay.achievement', ['ces', 'three', 'engine.entity-cache'])
-	.config(['$componentsProvider', function ($componentsProvider) {
-		'use strict';
+    .config(['$componentsProvider', function($componentsProvider) {
+        'use strict';
 
-		$componentsProvider.addComponentData({
-			'achievement': {
-				type: 'area',
-				radius: 5
-			}
-		});
-	}])
-	.factory('AchievementSystem', ['System', 'THREE', '$entityCache', function (System, THREE, $entityCache) {
-		'use strict';
+        $componentsProvider.addComponentData({
+            'achievement': {
+                type: 'area',
+                radius: 5
+            }
+        });
+    }])
+    .factory('AchievementSystem', ['System', 'THREE', '$entityCache', function(System, THREE, $entityCache) {
+        'use strict';
 
-		var AchievementSystem = System.extend({
-			addedToWorld: function (world) {
-				var sys = this;
+        var AchievementSystem = System.extend({
+            addedToWorld: function(world) {
+                var sys = this;
 
-				sys._super(world);
+                sys._super(world);
 
-				world.entityAdded('achievement').add(function (entity) {
-					var achievementData = entity.getComponent('achievement'),
-						achievement;
+                world.entityAdded('achievement').add(function(entity) {
+                    var achievementData = entity.getComponent('achievement'),
+                        achievement;
 
-					achievementData.achievement = achievement;
-				});
+                    achievementData.achievement = achievement;
+                });
 
-				this.achieved = {};
-			},
-			achieve: function (name) {
+                this.achieved = {};
+            },
+            achieve: function(achievementData) {
 
-				if (!this.achieved[name]) {
-					this.achieved[name] = true;
+                if (!this.achieved[achievementData.name]) {
+                    this.achieved[achievementData.name] = true;
 
-					Meteor.call('achieve', {
-						name: name
-					});
-				}
+                    Meteor.call('achieve', achievementData);
+                }
 
-			},
-			update: function () {
-				var world = this.world;
+            },
+            update: function() {
+                var world = this.world;
 
-				var mainPlayer = $entityCache.get('mainPlayer');
+                var mainPlayer = $entityCache.get('mainPlayer');
 
-				var self = this;
+                var self = this;
 
-				// Kind of weird...we'll only have one scene?
-				var achievementEntities = this.world.getEntities('achievement');
+                var achievementEntities = world.getEntities('achievement');
 
-				achievementEntities.forEach(function(entity) {
-					if ( mainPlayer ) {
-						var achievementData = entity.getComponent('achievement');
-						if (achievementData.type === 'area') {
-							if (mainPlayer.position.clone().sub(entity.position).lengthSq() < achievementData.radius * achievementData.radius) {
-								self.achieve(entity.name);
-							}
-						}
-					}
-				});
-			}
-		});
+                achievementEntities.forEach(function(entity) {
+                    if (mainPlayer) {
+                        var achievementData = entity.getComponent('achievement');
+                        if (achievementData.type === 'area') {
+                            if (mainPlayer.position.clone().sub(entity.position).lengthSq() < achievementData.radius * achievementData.radius) {
+                                self.achieve({
+                                    name: entity.name,
+                                    level: entity.level
+                                });
+                            }
+                        }
+                    }
+                });
+            }
+        });
 
-		return AchievementSystem;
-	}]);
+        return AchievementSystem;
+    }]);
--- server/methods/achievement.js
+++ server/methods/achievement.js
@@ -4,47 +4,46 @@
 var validator = Meteor.npmRequire('validator');
 
 Meteor.methods({
-	achieve: function (data) {
+    achieve: function(data) {
 
-		var user = Meteor.user();
+        var user = Meteor.user();
 
-		var character = Entities.findOne({
-		    active: true,
-		    owner: user._id
-		});
+        var character = Entities.findOne({
+            active: true,
+            owner: user._id
+        });
 
-		if (!character) {
-			throw new Meteor.Error('noActiveCharacterForAchievement');
-		}
+        if (!character) {
+            throw new Meteor.Error('noActiveCharacterForAchievement');
+        }
 
-		var entity = Entities.findOne({
-			name: data.name
-		});
+        var entity = Entities.findOne({
+            name: data.name
+        });
 
-		if (!entity) {
-			throw new Meteor.Error('noEntityForAchievement');
-		}
+        if (!entity) {
+            throw new Meteor.Error('noEntityForAchievement');
+        }
 
+        // TODO check if the achievement exists on the level
+        if (Collections.Achievements.find({
+                owner: character._id,
+                name: data.name
+            }).count() !== 0) {
+            throw new Meteor.Error('achievementExists');
+        }
+        // TODO check for distance
 
-		// TODO check if the achievement exists on the level
-		if (Collections.Achievements.find({
-			owner: character._id,
-			name: data.name
-		}).count() !== 0) {
-			throw new Meteor.Error('achievementExists');
-		}
-		// TODO check for distance
+        Collections.Achievements.insert({
+            owner: character._id,
+            name: data.name,
+            collectDate: new Date(),
+            level: data.level
+        });
 
+        if (entity.userData && entity.userData.message) {
+            Meteor.call('chatAnnounce', entity.userData.message.replace('{{ charname }}', character.name));
+        }
 
-		Collections.Achievements.insert({
-			owner: character._id,
-			name: data.name
-		});
-
-
-		if (entity.userData && entity.userData.message) {
-			Meteor.call('chatAnnounce', entity.userData.message.replace('{{ charname }}', character.name));
-		}
-
-	}
+    }
 });
