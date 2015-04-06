801756718a3479f8f08d1a38b99a774007015942
Ben Sparks <bsparks@unicon.net>
2015-04-06 09:16:19 -0700

    file cleanup, array notation just in case

--- client/game/ui/directives/ngEnter.js
+++ client/game/ui/directives/ngEnter.js
@@ -1,14 +1,17 @@
 angular.module('game.ui.directives')
-	.directive('ngEnter', function() {
-		return function(scope, element, attrs) {
-			element.bind('keydown keypress', function(event) {
-				if(event.which === 13) {
-						scope.$apply(function(){
-								scope.$eval(attrs.ngEnter);
-						});
+    .directive('ngEnter', [
+        function() {
+            'use strict';
+            return function(scope, element, attrs) {
+                element.bind('keydown keypress', function(event) {
+                    if (event.which === 13) {
+                        scope.$apply(function() {
+                            scope.$eval(attrs.ngEnter);
+                        });
 
-						event.preventDefault();
-				}
-			});
-		};
-});
+                        event.preventDefault();
+                    }
+                });
+            };
+        }
+    ]);
