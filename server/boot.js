f9cf0f7f08295c924b832ee0258556135a544e0b
Ben Sparks <bsparks@unicon.net>
2015-04-16 23:27:40 -0700

    some stuff to try to debug the server, deferred not getting a resolution!

--- server/boot.js
+++ server/boot.js
@@ -2,5 +2,5 @@
 
 Meteor.startup(function() {
     // this will boot up the whole server Game Engine
-    angular.injector(['IronbaneServer']);
+    global.engine = angular.injector(['IronbaneServer']);
 });
