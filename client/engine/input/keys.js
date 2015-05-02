cff01110d0dadbaa247a8916eda75ec85b3111cd
Ben Sparks <bsparks@unicon.net>
2015-05-02 00:04:21 -0700

    killed an old TODO for keyboard keys

--- client/engine/input/keys.js
+++ client/engine/input/keys.js
@@ -1,53 +1,723 @@
 angular.module('engine.input.keys', [])
-    .constant('KEYS', {
-        // TODO: fill rest out
-        ENTER: 13,
-        SHIFT: 16,
-        ESC: 27,
-
-        SPACE: 32,
-
-        LEFT: 37,
-        UP: 38,
-        RIGHT: 39,
-        DOWN: 40,
-
-        A: 65,
-        B: 66,
-        C: 67,
-        D: 68,
-        E: 69,
-        F: 70,
-        G: 71,
-        H: 72,
-        I: 73,
-        J: 74,
-        K: 75,
-        L: 76,
-        M: 77,
-        N: 78,
-        O: 79,
-        P: 80,
-        Q: 81,
-        R: 82,
-        S: 83,
-        T: 84,
-        U: 85,
-        V: 86,
-        W: 87,
-        X: 88,
-        Y: 89,
-        Z: 90,
-
-        _0: 48,
-        _1: 49,
-        _2: 50,
-        _3: 51,
-        _4: 52,
-        _5: 53,
-        _6: 54,
-        _7: 55,
-        _8: 56,
-        _9: 57,
-
-    });
+    .constant('KEYS', Object.freeze(Object.create(null, {
+        BACKSPACE: {
+            value: 8,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        TAB: {
+            value: 9,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        ENTER: {
+            value: 13,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        SHIFT: {
+            value: 16,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        CTRL: {
+            value: 17,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        ALT: {
+            value: 18,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        BREAK: {
+            value: 19,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        PAUSE: {
+            value: 19,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        CAPS_LOCK: {
+            value: 20,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        ESC: {
+            value: 27,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        SPACE: {
+            value: 32,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        PAGE_UP: {
+            value: 33,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        PAGE_DOWN: {
+            value: 34,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        END: {
+            value: 35,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        HOME: {
+            value: 36,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        LEFT: {
+            value: 37,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        UP: {
+            value: 38,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        RIGHT: {
+            value: 39,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        DOWN: {
+            value: 40,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        INSERT: {
+            value: 45,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        DELETE: {
+            value: 46,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        '0': {
+            value: 48,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        _0: {
+            value: 48,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        ZERO: {
+            value: 48,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        '1': {
+            value: 49,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        _1: {
+            value: 49,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        ONE: {
+            value: 49,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        '2': {
+            value: 50,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        _2: {
+            value: 50,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        TWO: {
+            value: 50,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        '3': {
+            value: 51,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        _3: {
+            value: 51,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        THREE: {
+            value: 51,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        '4': {
+            value: 52,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        _4: {
+            value: 52,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        FOUR: {
+            value: 52,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        '5': {
+            value: 53,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        _5: {
+            value: 53,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        FIVE: {
+            value: 53,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        '6': {
+            value: 54,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        _6: {
+            value: 54,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        SIX: {
+            value: 54,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        '7': {
+            value: 55,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        _7: {
+            value: 55,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        SEVEN: {
+            value: 55,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        '8': {
+            value: 56,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        _8: {
+            value: 56,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        EIGHT: {
+            value: 56,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        '9': {
+            value: 57,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        _9: {
+            value: 57,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        NINE: {
+            value: 57,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        A: {
+            value: 65,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        B: {
+            value: 66,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        C: {
+            value: 67,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        D: {
+            value: 68,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        E: {
+            value: 69,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        F: {
+            value: 70,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        G: {
+            value: 71,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        H: {
+            value: 72,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        I: {
+            value: 73,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        J: {
+            value: 74,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        K: {
+            value: 75,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        L: {
+            value: 76,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        M: {
+            value: 77,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        N: {
+            value: 78,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        O: {
+            value: 79,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        P: {
+            value: 80,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        Q: {
+            value: 81,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        R: {
+            value: 82,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        S: {
+            value: 83,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        T: {
+            value: 84,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        U: {
+            value: 85,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        V: {
+            value: 86,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        W: {
+            value: 87,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        X: {
+            value: 88,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        Y: {
+            value: 89,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        Z: {
+            value: 90,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        LEFT_WINDOW_KEY: {
+            value: 91,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        RIGHT_WINDOW_KEY: {
+            value: 92,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        SELECT_KEY: {
+            value: 93,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        NUMPAD_0: {
+            value: 96,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        NUMPAD_1: {
+            value: 97,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        NUMPAD_2: {
+            value: 98,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        NUMPAD_3: {
+            value: 99,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        NUMPAD_4: {
+            value: 100,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        NUMPAD_5: {
+            value: 101,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        NUMPAD_6: {
+            value: 102,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        NUMPAD_7: {
+            value: 103,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        NUMPAD_8: {
+            value: 104,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        NUMPAD_9: {
+            value: 105,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        MULTIPLY: {
+            value: 106,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        ADD: {
+            value: 107,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        SUBTRACT: {
+            value: 109,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        DECIMAL_POINT: {
+            value: 110,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        DIVIDE: {
+            value: 111,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        F1: {
+            value: 112,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        F2: {
+            value: 113,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        F3: {
+            value: 114,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        F4: {
+            value: 115,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        F5: {
+            value: 116,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        F6: {
+            value: 117,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        F7: {
+            value: 118,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        F8: {
+            value: 119,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        F9: {
+            value: 120,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        F10: {
+            value: 121,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        F11: {
+            value: 122,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        F12: {
+            value: 123,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        NUM_LOCK: {
+            value: 144,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        SCROLL_LOCK: {
+            value: 145,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        SEMICOLON: {
+            value: 186,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        EQUAL_SIGN: {
+            value: 187,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        COMMA: {
+            value: 188,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        DASH: {
+            value: 189,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        PERIOD: {
+            value: 190,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        FORWARD_SLASH: {
+            value: 191,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        GRAVE_ACCENT: {
+            value: 192,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        OPEN_BRACKET: {
+            value: 219,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        BACK_SLASH: {
+            value: 220,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        CLOSE_BRACKET: {
+            value: 221,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        },
+        SINGLE_QUOTE: {
+            value: 222,
+            configurable: false,
+            enumerable: true,
+            writable: false
+        }
+    })));
