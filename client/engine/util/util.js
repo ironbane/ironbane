c423684d4e14589b8fc5bc247c0e8cac89eabd17
Nick Janssen <info@nickjanssen.com>
2015-05-05 12:21:08 +0900

    convert waitForMeteorGuestUserLogin to promise

--- client/engine/util/util.js
+++ client/engine/util/util.js
@@ -1,6 +1,6 @@
 angular
     .module('engine.util', [])
-    .service('Util', [function() {
+    .service('Util', ['$q', function($q) {
         'use strict';
 
         this.getRandomInt = function(min, max) {
@@ -20,16 +20,18 @@ angular
 
         // The meteor-accounts-guest package is sometimes too slow in logging us in
         // that's why we need to check here if we have an assigned guest user
-        this.waitForMeteorGuestUserLogin = function(fn) {
+        this.waitForMeteorGuestUserLogin = function() {
+        	var deferred = $q.defer();
             var doCheck = function() {
                 var user = Meteor.user();
                 if (user) {
-                    fn();
+                    deferred.resolve(user);
                 } else {
                     Meteor.setTimeout(doCheck, 10);
                 }
             };
 
             Meteor.setTimeout(doCheck, 10);
+            return deferred.promise;
         };
     }]);
