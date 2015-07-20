4ae3edc407b99305ecc6c39df186eb56266969a8
Nick Janssen <info@nickjanssen.com>
2015-07-20 20:41:18 +0900

    server accounts: create profile object by default, add method to update profile

--- server/services/accounts.js
+++ server/services/accounts.js
@@ -14,5 +14,29 @@ angular
                 AccountsCollection.removeOldGuests(before);
             }, 3600 * 1000);
 
+            // Default player settings
+            Accounts.onCreateUser(function(options, user) {
+
+                // We're enforcing at least an empty profile object to avoid needing to check
+                // for its existence later.
+                user.profile = options.profile ? options.profile : {};
+
+                user.profile.enableSound = true;
+
+                return user;
+            });           
+
+            Meteor.methods({
+                updateProfile: function (field, value) {
+                    if (this.userId) {
+                        var fieldsToUpdate = {};
+                        fieldsToUpdate['profile.' + field] = value;
+                        Meteor.users.update(this.userId, {
+                            $set: fieldsToUpdate
+                        });
+                    }
+                }
+            }) 
+
         }
     ]);
