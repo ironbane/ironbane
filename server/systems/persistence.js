68a22adceb3b5c63fec938de308d9e705b9f1184
Nick Janssen <info@nickjanssen.com>
2015-09-07 23:04:39 +0900

    persistence: persist on logout

--- server/systems/persistence.js
+++ server/systems/persistence.js
@@ -12,28 +12,40 @@ angular
         function(System, EntitiesCollection, Timer, $log) {
             'use strict';
 
+            var persist = function (entity) {
+                var persist = entity.getComponent('persisted'),
+                    entityState = entity.toJSON();
+
+                entityState.position = entity.position.serialize();
+                entityState.rotation = entity.rotation.serialize();
+
+                //$log.debug('persist: ', entityState);
+
+                EntitiesCollection.update({
+                    _id: persist._id
+                }, {
+                    $set: entityState
+                });
+            };
+
             var PersistSystem = System.extend({
                 init: function() {
                     this.timer = new Timer(20);
                 },
+                addedToWorld: function(world) {
+                    this._super(world);
+
+                    world.singleEntityRemoved.add(function (entity) {
+                        if (entity.getComponent('persisted')) {
+                            persist(entity);
+                        }
+                    });
+
+                },
                 update: function() {
                     if (this.timer.isExpired) {
                         var entsToPersist = this.world.getEntities('persisted');
-                        entsToPersist.forEach(function(entity) {
-                            var persist = entity.getComponent('persisted'),
-                                entityState = entity.toJSON();
-
-                            entityState.position = entity.position.serialize();
-                            entityState.rotation = entity.rotation.serialize();
-
-                            //$log.debug('persist: ', entityState);
-
-                            EntitiesCollection.update({
-                                _id: persist._id
-                            }, {
-                                $set: entityState
-                            });
-                        });
+                        entsToPersist.forEach(persist);
 
                         this.timer.reset();
                     }
