cf5b5b1a609d87f855d4070a618eddfc60382632
Ben Sparks <bsparks@unicon.net>
2015-09-16 13:55:59 -0700

    semi colons and console error

--- shared/systems/actor.ng.js
+++ shared/systems/actor.ng.js
@@ -4,40 +4,39 @@ angular
         'game.ai.states'
     ])
     .factory('ActorSystem', function($log, System, States) {
-            'use strict';
+        'use strict';
 
-            var stateNames = [Meteor.isClient ? 'localState' : 'globalState']
+        var stateNames = [Meteor.isClient ? 'localState' : 'globalState'];
 
-            return System.extend({
-                addedToWorld: function(world) {
-                    this._super(world);
+        return System.extend({
+            addedToWorld: function(world) {
+                this._super(world);
 
-                    stateNames.forEach(function (stateName) {
-                        world.entityAdded(stateName).add(function(entity) {
-                            var stateComponent = entity.getComponent(stateName);
-                            stateComponent._state = new States.get(stateComponent.state, entity, stateComponent.config, world);
-                        });
-                        world.entityRemoved(stateName).add(function(entity) {
-                            var stateComponent = entity.getComponent(stateName);
-                            stateComponent._state.destroy();
-                        });
-                    })
-                },
-                update: function(dTime) {
-                    var me = this;
+                stateNames.forEach(function(stateName) {
+                    world.entityAdded(stateName).add(function(entity) {
+                        var stateComponent = entity.getComponent(stateName);
+                        stateComponent._state = new States.get(stateComponent.state, entity, stateComponent.config, world);
+                    });
+                    world.entityRemoved(stateName).add(function(entity) {
+                        var stateComponent = entity.getComponent(stateName);
+                        stateComponent._state.destroy();
+                    });
+                });
+            },
+            update: function(dTime) {
+                var me = this;
 
-                    stateNames.forEach(function (stateName) {
-                        var stateEntities = me.world.getEntities(stateName);
+                stateNames.forEach(function(stateName) {
+                    var stateEntities = me.world.getEntities(stateName);
 
-                        stateEntities.forEach(function(entity) {
-                            var stateComponent = entity.getComponent(stateName);
+                    stateEntities.forEach(function(entity) {
+                        var stateComponent = entity.getComponent(stateName);
 
-                            if (stateComponent) {
-                                stateComponent._state.update(dTime);
-                            }
-                        });
-                    })
-                }
-            });
-        }
-    );
+                        if (stateComponent && stateComponent._state) {
+                            stateComponent._state.update(dTime);
+                        }
+                    });
+                });
+            }
+        });
+    });
