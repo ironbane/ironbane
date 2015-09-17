098526c37d1497735bd035bb1a879301af8b0667
Ben Sparks <bsparks@unicon.net>
2015-09-17 00:32:53 -0700

    CES: shared components capability (optimization)

--- shared/ces/components-registry.js
+++ shared/ces/components-registry.js
@@ -2,10 +2,15 @@ angular.module('ces.components-registry', ['ces.component'])
     .provider('$components', function() {
         'use strict';
 
-        var _preload = {};
+        var _preload = {},
+            _preloadShared = {};
 
         this.register = function(data) {
             angular.extend(_preload, data);
+        };
+        // TODO: prevent duplicate name registrations across collections
+        this.registerShared = function(data) {
+            angular.extend(_preloadShared, data);
         };
 
         this.$get = [
@@ -13,6 +18,7 @@ angular.module('ces.components-registry', ['ces.component'])
             '$log',
             function(Component, $log) {
                 var _components = {},
+                    _sharedComponents = {},
                     _initialized = false,
                     svc = function() {};
 
@@ -21,9 +27,14 @@ angular.module('ces.components-registry', ['ces.component'])
                         return;
                     }
 
-                    for (var key in _preload) {
-                        this.registerComponent(key, _preload[key]);
+                    for (var componentName in _preload) {
+                        this.registerComponent(componentName, _preload[componentName]);
                     }
+
+                    for (var sComponentName in _preloadShared) {
+                        this.registerSharedComponent(sComponentName, _preloadShared[sComponentName]);
+                    }
+
                     _initialized = true;
                 };
 
@@ -32,20 +43,33 @@ angular.module('ces.components-registry', ['ces.component'])
                     _components[name] = data;
                 };
 
+                svc.registerSharedComponent = function(name, data) {
+                    // a shared component is basically a singleton instance,
+                    // there should be no reason all entities can't share the same reference
+                    _sharedComponents[name] = new Component(name, data);
+                };
+
                 svc.get = function(name, data) {
                     var c;
+
                     try {
-                        if (!_components[name]) {
+                        if (_components[name]) {
+                            c = new Component(name, _components[name]);
+                        }
+
+                        if (!c && _sharedComponents[name]) {
+                            c = _sharedComponents[name];
+                        }
+
+                        if (!c) {
                             throw new Error('Component ' + name + ' not registered!');
+                        } else {
+                            for (var prop in data) {
+                                c[prop] = data[prop];
+                            }
+
+                            c.setIsDirty(false);
                         }
-
-                        c = new Component(name, _components[name]);
-
-                        for (var prop in data) {
-                            c[prop] = data[prop];
-                        }
-
-                        c.setIsDirty(false);
                     } catch (err) {
                         $log.debug('error instantiating component: ', err);
                     }
