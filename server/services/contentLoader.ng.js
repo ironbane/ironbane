3d8ed692c304e54c8271cd55e9c930ebdf6be4e1
Ben Sparks <bsparks@unicon.net>
2015-09-23 00:41:01 -0700

    content: choose csv load method based on file path rather than dev vs prod; enable config override using Meteor.settings

--- server/services/contentLoader.ng.js
+++ server/services/contentLoader.ng.js
@@ -3,7 +3,7 @@ angular
         'global.constants',
         'engine.util'
     ])
-    .service('ContentLoader', function (IB_CONSTANTS, $q, IbUtils) {
+    .service('ContentLoader', function(IB_CONSTANTS, $q, IbUtils) {
         'use strict';
 
         var parse = Meteor.npmRequire('csv-parse');
@@ -18,15 +18,18 @@ angular
         var itemHeaders = null;
         var npcHeaders = null;
 
-        var getValue = function (row, headers, header) {
+        var getValue = function(row, headers, header) {
             return row[headers.indexOf(header)];
         };
 
-        var loadContent = function (remoteUrl, assetsName) {
+        var loadContent = function(filename) {
             var deferred = $q.defer();
 
-            var load = function (text) {
-                var parser = parse(text, {delimiter: ',', auto_parse: true}, function(err, data){
+            var load = function(text) {
+                var parser = parse(text, {
+                    delimiter: ',',
+                    auto_parse: true
+                }, function(err, data) {
                     if (err) {
                         deferred.reject(err);
                     }
@@ -37,22 +40,22 @@ angular
                 return parser;
             };
 
-            if (IB_CONSTANTS.isDev) {
-                request(remoteUrl, function (error, response, body) {
+            // assume if there's an http protocol that we're using remote files,
+            // otherwise in the privates
+            if (filename.search(/http/) >= 0) {
+                request(filename, function(error, response, body) {
                     if (!error && response.statusCode === 200) {
                         load(body);
-                    }
-                    else {
+                    } else {
                         deferred.reject(error);
                     }
                 });
-            }
-            else {
-                // On production we load a static file so we're in control of the changes each release
-                Assets.getText(assetsName, function (err, text) {
+            } else {
+                Assets.getText(filename, function(err, text) {
                     if (err) {
-                        throw err;
+                        return deferred.reject(err);
                     }
+
                     load(text);
                 });
             }
@@ -60,7 +63,7 @@ angular
             return deferred.promise;
         };
 
-        this.buildInventory = function (inventoryItemNames, equipmentItemNames) {
+        this.buildInventory = function(inventoryItemNames, equipmentItemNames) {
 
             inventoryItemNames = inventoryItemNames || [];
             equipmentItemNames = equipmentItemNames || [];
@@ -70,8 +73,8 @@ angular
             var handSlotCount = 0;
             var relicCount = 1; // Relics apparently start count from 1 while slots start from 0 :P
 
-            var addItemToInventory = function (addToEquipment) {
-                return function (itemName) {
+            var addItemToInventory = function(addToEquipment) {
+                return function(itemName) {
                     itemName = itemName.trim();
 
                     var regex = /%[0-9]+/;
@@ -81,7 +84,7 @@ angular
                         itemName = itemName.replace(regexResult, '').trim();
                     }
 
-                    rawItems.forEach(function (item) {
+                    rawItems.forEach(function(item) {
                         var name = getValue(item, itemHeaders, 'Name');
 
                         if (name === itemName) {
@@ -93,27 +96,21 @@ angular
                                 if ((type === 'weapon' || type === 'shield') && handSlotCount <= 1) {
                                     if (handSlotCount === 0) {
                                         slot = 'rhand';
-                                    }
-                                    else if (handSlotCount === 1) {
+                                    } else if (handSlotCount === 1) {
                                         slot = 'lhand';
                                     }
                                     handSlotCount++;
-                                }
-                                else if (type === 'relic' && relicCount <= 4) {
+                                } else if (type === 'relic' && relicCount <= 4) {
                                     slot = 'relic' + relicCount;
                                     relicCount++;
-                                }
-                                else if (type === 'body' && !inventory['body']) {
+                                } else if (type === 'body' && !inventory['body']) {
                                     slot = 'body';
-                                }
-                                else if (type === 'feet' && !inventory['feet']) {
+                                } else if (type === 'feet' && !inventory['feet']) {
                                     slot = 'feet';
-                                }
-                                else if (type === 'head' && !inventory['head']) {
+                                } else if (type === 'head' && !inventory['head']) {
                                     slot = 'head';
                                 }
-                            }
-                            else {
+                            } else {
                                 if (slotCount <= 7) {
                                     slot = 'slot' + slotCount;
                                     slotCount++;
@@ -141,7 +138,7 @@ angular
                                     dropChance: 'Drop Chance %'
                                 };
 
-                                _.each(mapping, function (val, key) {
+                                _.each(mapping, function(val, key) {
                                     var mappedValue = getValue(item, itemHeaders, val);
                                     if (mappedValue || mappedValue === 0) {
                                         inventory[slot][key] = mappedValue;
@@ -158,8 +155,8 @@ angular
 
                         }
                     });
-                }
-            }
+                };
+            };
 
             equipmentItemNames.forEach(addItemToInventory(true));
             inventoryItemNames.forEach(addItemToInventory(false));
@@ -167,10 +164,8 @@ angular
             return inventory;
         };
 
-        this.buildNPCPrefabs = function () {
-            var me = this;
-
-            rawNpcs.forEach(function (npc) {
+        this.buildNPCPrefabs = function() {
+            rawNpcs.forEach(function(npc) {
 
                 // For now only set the names
                 // We'll build the inventory when fetching the prefab to ensure unique uuid's
@@ -178,11 +173,11 @@ angular
                 var equipmentItemNames = getValue(npc, npcHeaders, 'equipment').split(',');
 
                 var monsterBehaviour = getValue(npc, npcHeaders, 'behaviour').split(',');
-                monsterBehaviour = monsterBehaviour.map(function (p) {
+                monsterBehaviour = monsterBehaviour.map(function(p) {
                     return p.trim();
                 });
                 var monsterBehaviourHash = {};
-                _.each(monsterBehaviour, function (p) {
+                _.each(monsterBehaviour, function(p) {
                     if (p) {
                         monsterBehaviourHash[p] = true;
                     }
@@ -272,10 +267,26 @@ angular
         this.load = function() {
             var me = this;
 
-            return $q.all([
-                loadContent('https://docs.google.com/spreadsheets/d/1a_dtZT1dUZnA3DlFTWbGKBCUZIAhuy2NQ6Hj8wtZJdU/pub?output=csv', 'npcs.csv'),
-                loadContent('https://docs.google.com/spreadsheets/d/1ZC-ydW7if6Ci0TytsSLaio0LMoCntQwaUkXAOwjn7Y8/pub?output=csv', 'items.csv')
-            ]).then(function(result) {
+            // TODO: rely on a better config file than hard coding these...
+            var defaultProdFiles = [
+                'npcs.csv',
+                'items.csv'
+            ],
+            defaultDevFiles = [
+                'https://docs.google.com/spreadsheets/d/1a_dtZT1dUZnA3DlFTWbGKBCUZIAhuy2NQ6Hj8wtZJdU/pub?output=csv', // npcs
+                'https://docs.google.com/spreadsheets/d/1ZC-ydW7if6Ci0TytsSLaio0LMoCntQwaUkXAOwjn7Y8/pub?output=csv' // items
+            ],
+            filesToLoad = IB_CONSTANTS.isDev ? defaultDevFiles : defaultProdFiles;
+
+            // allow override via commandline or env variable
+            if (Meteor.settings.content) {
+                filesToLoad = [
+                    Meteor.settings.content.npcs,
+                    Meteor.settings.content.items
+                ];
+            }
+
+            return $q.all(filesToLoad.map(function(file) { return loadContent(file); })).then(function(result) {
                 var npcs = result[0],
                     items = result[1];
 
@@ -292,41 +303,41 @@ angular
             });
         };
 
-        this.hasNPCPrefab = function (prefabName) {
+        this.hasNPCPrefab = function(prefabName) {
             return npcPrefabs[prefabName];
         };
 
-        this.getNPCPrefab = function (prefabName) {
+        this.getNPCPrefab = function(prefabName) {
             var prefab = JSON.parse(JSON.stringify(npcPrefabs[prefabName]));
 
             if (prefab.components.quad.charBuildData.special === 'villager') {
 
 
-                var hairList = _.filter(IB_CONSTANTS.charImages.hair, function (img) {
+                var hairList = _.filter(IB_CONSTANTS.charImages.hair, function(img) {
                     return img >= 1000 && img <= 1015;
                 });
-                var eyesList = _.filter(IB_CONSTANTS.charImages.eyes, function (img) {
+                var eyesList = _.filter(IB_CONSTANTS.charImages.eyes, function(img) {
                     return img >= 1000 && img <= 1015;
                 });
-                var skinList = _.filter(IB_CONSTANTS.charImages.skin, function (img) {
+                var skinList = _.filter(IB_CONSTANTS.charImages.skin, function(img) {
                     return img >= 1000 && img <= 1015;
                 });
 
                 prefab.components.quad.charBuildData = {
-                    body: _.sample([1,10,102,12,16,19,2,23,3,4,81,82,83,95,97,99]),
+                    body: _.sample([1, 10, 102, 12, 16, 19, 2, 23, 3, 4, 81, 82, 83, 95, 97, 99]),
                     feet: _.sample(IB_CONSTANTS.charImages.feet),
-                    head: _.sample([0,0,0,0,0,0,0,1,2,4,5,6,7,8]),
+                    head: _.sample([0, 0, 0, 0, 0, 0, 0, 1, 2, 4, 5, 6, 7, 8]),
                     hair: _.sample(hairList),
                     eyes: _.sample(eyesList),
                     skin: _.sample(skinList)
-                }
+                };
 
                 // console.log(prefab.components.quad.charBuildData);
 
                 delete prefab.components.inventory;
 
-                prefab.components.steeringBehaviour.speed = IbUtils.getRandomInt(1,2);
-                prefab.components.steeringBehaviour.maxSpeed = IbUtils.getRandomInt(1,2);
+                prefab.components.steeringBehaviour.speed = IbUtils.getRandomInt(1, 2);
+                prefab.components.steeringBehaviour.maxSpeed = IbUtils.getRandomInt(1, 2);
 
                 // prefab.itemNames = [];
 
