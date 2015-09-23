e20e6a82ac3b1d4508923055b11884605021cc4f
Nick Janssen <info@nickjanssen.com>
2015-09-23 12:36:03 +0900

    recast: disable for now

--- shared/engine/patrol.ng.js
+++ shared/engine/patrol.ng.js
@@ -13,47 +13,47 @@ angular
         _.extend(this, {
             buildNodes: function(zone, mesh) {
 
-                var deferred = $q.defer();
+                // var deferred = $q.defer();
 
-                recastPromises[zone] = deferred.promise;
+                // recastPromises[zone] = deferred.promise;
 
-                new Recast('lib/recast.js', function(recast){
+                // new Recast('lib/recast.js', function(recast){
 
-                    recast.set_cellSize(0.25);
-                    recast.set_cellHeight(0.2);
-                    recast.set_agentHeight(0.1);
-                    recast.set_agentRadius(0.2);
-                    recast.set_agentMaxClimb(4.0);
-                    recast.set_agentMaxSlope(45.0);
+                //     recast.set_cellSize(0.25);
+                //     recast.set_cellHeight(0.2);
+                //     recast.set_agentHeight(0.1);
+                //     recast.set_agentRadius(0.2);
+                //     recast.set_agentMaxClimb(4.0);
+                //     recast.set_agentMaxSlope(45.0);
 
-                    var objContents = ObjExporter.parse(mesh);
+                //     var objContents = ObjExporter.parse(mesh);
 
-                    // console.log(objContents);
+                //     // console.log(objContents);
 
-                    recast.OBJDataLoader(objContents, recast.cb(function () {
+                //     recast.OBJDataLoader(objContents, recast.cb(function () {
 
-                        recast.buildSolo();
+                //         recast.buildSolo();
 
-                        // recast.buildSolo(null, recast.cb(function () {
+                //         // recast.buildSolo(null, recast.cb(function () {
 
-                            // console.log(arguments);
-                        // recastPromises[zone] = recast;
+                //             // console.log(arguments);
+                //         // recastPromises[zone] = recast;
 
-                            // console.log('Init recast for zone ' + zone);
+                //             // console.log('Init recast for zone ' + zone);
 
-                            // setInterval(function () {
-                            recast.getRandomPoint(recast.cb(function(pt1x, pt1y, pt1z) {
-                                deferred.resolve(recast);
-                                // alert(pt1x, pt1y, pt1z);
-                            }));
-                            // }, 5000);
+                //             // setInterval(function () {
+                //             recast.getRandomPoint(recast.cb(function(pt1x, pt1y, pt1z) {
+                //                 deferred.resolve(recast);
+                //                 // alert(pt1x, pt1y, pt1z);
+                //             }));
+                //             // }, 5000);
 
-                        // }));
+                //         // }));
 
 
-                    }));
+                //     }));
 
-                });
+                // });
 
             },
             findPath: function(startPosition, targetPosition, zone, group) {
