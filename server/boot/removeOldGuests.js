018c547d8c2426b1dd2fe2a2c49982e6f8f4f4d3
Nick Janssen <info@nickjanssen.com>
2015-10-17 09:20:44 +0900

    removeOldGuests: fix not working, remove obselete entities

--- server/boot/removeOldGuests.js
+++ server/boot/removeOldGuests.js
@@ -4,15 +4,29 @@ angular
     ])
     .run([
         'AccountsCollection',
-        function(AccountsCollection) {
+        'EntitiesCollection',
+        function(AccountsCollection, EntitiesCollection) {
             'use strict';
 
-            Meteor.setInterval(function() {
-                /* clean out all guest accounts more than 1 hour old */
+            var clear = function () {
+                // Clean out all guest accounts more than 1 hour old
                 var before = new Date();
                 before.setHours(before.getHours() - 1);
-                AccountsCollection.removeOldGuests(before);
-            }, 3600 * 1000);
+                Accounts.removeOldGuests(before);
+
+                // Remove entities that belong to guests and are no longer there
+                var entities = EntitiesCollection.find({});
+                entities.forEach(function(row) {
+                    var user = Meteor.users.findOne(row.owner);
+
+                    if (!user || (user.profile.guest && !user.status.online)) {
+                        EntitiesCollection.remove(row._id);
+                    }
+                });
+            };
+
+            Meteor.startup(clear);
+            Meteor.setInterval(clear, 3600 * 1000);
 
         }
     ]);
