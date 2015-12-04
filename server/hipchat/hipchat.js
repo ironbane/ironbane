22520ed9f3a59383fa167812e1b3911e628c39c6
Nick Janssen <info@nickjanssen.com>
2015-12-04 16:14:51 +0900

    hipchat: make configurable/optional

--- server/hipchat/hipchat.js
+++ server/hipchat/hipchat.js
@@ -3,32 +3,36 @@ angular
     .service('HipChat', function() {
         'use strict';
 
-        var hipchat = Meteor.npmRequire('node-hipchat');
+        if (Meteor.settings.server.hipchat.apiKey) {
+            var hipchat = Meteor.npmRequire('node-hipchat');
 
-        var HC = new hipchat('817d6f5dbb025e4f0b485aca5733d5');
+            var HC = new hipchat(Meteor.settings.server.hipchat.apiKey);
 
-        var rooms = null;
+            var rooms = null;
 
-        HC.listRooms(function(data) {
-            rooms = data.rooms;
-        });
+            HC.listRooms(function(data) {
+                rooms = data.rooms;
+            });
+        }
 
         this.postMessage = function(roomName, msg) {
-            var room = _.findWhere(rooms, {
-                name: roomName
-            });
-
-            if (room) {
-                var params = {
-                    room: room.room_id,
-                    from: 'Ironbot',
-                    message: msg,
-                    color: 'green'
-                };
-
-                HC.postMessage(params, function(data) {
-
+            if (Meteor.settings.server.hipchat.apiKey) {
+                var room = _.findWhere(rooms, {
+                    name: roomName
                 });
+
+                if (room) {
+                    var params = {
+                        room: room.room_id,
+                        from: 'Ironbot',
+                        message: msg,
+                        color: 'green'
+                    };
+
+                    HC.postMessage(params, function(data) {
+
+                    });
+                }
             }
         };
 
--- shared/boot/settings.js
+++ shared/boot/settings.js
@@ -9,3 +9,5 @@ Meteor.settings.server = Meteor.settings.server || {};
 Meteor.settings.server.id = Meteor.settings.server.id || 'local';
 Meteor.settings.server.name = Meteor.settings.server.name || 'Local';
 Meteor.settings.server.capacity = Meteor.settings.server.capacity || 20;
+
+Meteor.settings.server.hipchat = Meteor.settings.server.hipchat || {};
